79dcdf720e38080f6f356789125d7dab6b5d6730
Fuhunter <fuhunter@t-online.de>
2014-10-27 19:57:01 +0100

    Initial commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class Application extends Controller {
+
+    public static Result index() {
+        return ok(index.render("Your new application is ready."));
+    }
+
+    public static Result about() {
+        return ok(about.render());
+    }
+
+    public static Result contact() {
+        return ok(contact.render());
+    }
+}
