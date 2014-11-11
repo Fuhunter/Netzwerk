256c2778953ea52ac8cdb84024ef9bff8b537e93
chritz <cg.gaudl@gmx.de>
2014-11-11 15:50:37 +0100

    Gruppen neu

---
+++ app/controllers/Groups.java
@@ -0,0 +1,38 @@
+/**
+ * @author Thomas Dennhardt, Christoph Gaudl and Niclas Günther
+ *
+ * Network Controller is index for logged in Users
+ */
+
+package controllers;
+
+import play.*;
+import play.data.DynamicForm;
+import play.db.ebean.Transactional;
+import play.mvc.*;
+import views.html.*;
+
+
+public class Groups extends Controller {
+
+    /**
+     * If authenticated render Indexpage
+     * @return
+     */
+    @Security.Authenticated(Secured.class)
+    public static Result index() {
+        return ok(groups.render(session().get("email"), "bla"));
+    }
+
+
+
+
+    @Security.Authenticated(Secured.class)
+    @Transactional
+    public static Result register(){
+        DynamicForm newGroupForm = new DynamicForm().bindFromRequest();
+        String gruppenname = newGroupForm.get("gruppenname");
+        String gruppenbeschreibung = newGroupForm.get("gruppenbeschreibung");
+        return ok(groups.render(session().get("email"), "Success"));
+    }
+}
