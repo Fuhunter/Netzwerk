467143d95bfa4d38f8b2f78e9ec27528e5e2d971
chritz <cg.gaudl@gmx.de>
2014-11-17 14:11:28 +0100

    Button zur Gruppenerstellung eingefügt

---
+++ app/models/Gruppenmitglieder.java
@@ -0,0 +1,49 @@
+/**
+ * @author Thomas Dennhardt, Christoph Gaudl and Niclas Günther
+ *
+ * User Database Model
+ */
+
+package models;
+
+import play.db.ebean.*;
+import play.data.validation.*;
+import java.util.*;
+import javax.persistence.*;
+
+@Entity
+public class Gruppenmitglieder extends Model {
+
+    @Id
+    protected Long groups;
+
+    @Id
+    protected Long users;
+
+    public Long getId() {
+        return (this.groups);
+    }
+
+    public void setId(Long ID) {
+        this.groups = ID;
+        this.users = ID;
+    }
+
+    public void setMember(Long users){
+        this.users = users;
+    }
+
+
+    public Long getMember(Long users){
+        return(this.users);
+    }
+
+    public Long getGroup(Long groups){
+        return(this.groups);
+    }
+
+    public void setGroup(Long groups){
+        this.groups = groups;
+    }
+
+}
