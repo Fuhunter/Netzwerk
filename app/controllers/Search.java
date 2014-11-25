0278dfb558525b7505bf1dd1b5128a4060837998
Fuhunter <fuhunter@t-online.de>
2014-11-25 15:30:56 +0100

    Suchen

--- app/controllers/Search.java
+++ app/controllers/Search.java
@@ -6,11 +6,15 @@
 
 package controllers;
 
+import com.avaje.ebean.ExpressionList;
+import models.Users;
 import play.*;
 import play.data.DynamicForm;
 import play.mvc.*;
 import views.html.*;
 
+import java.beans.Expression;
+import java.util.List;
 
 public class Search extends Controller {
 
@@ -21,7 +25,23 @@ public class Search extends Controller {
     public static Result search() {
         DynamicForm sform = new DynamicForm().bindFromRequest();
 
+        String checker = sform.get("check");
 
-        return ok(search.render(session().get("email"), false, ""));
+        List<Users> result = null;
+        if (checker.equals("echeck")) {
+            String email = "%" + sform.get("email") + "%";
+            result = Users.find.where().like("email", email);
+        } else if (checker.equals("vcheck")) {
+            String vname = sform.get("vname");
+            result = Users.find.where().like("vorname", vname);
+        } else if (checker.equals("ncheck")) {
+            String nname = sform.get("nname");
+            result = Users.find.where().like("nachname", nname);
+        } else {
+            return badRequest(search.render(session().get("email"), false, ""));
+        }
+
+        return ok(String.valueOf(result.size()));
+        //return ok(search.render(session().get("email"), false, ""));
     }
 }
